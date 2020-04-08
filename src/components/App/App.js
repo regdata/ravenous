44169bc52d704ac8a340c57b69a6f440a5600983
regdata <regdata.voice@gmail.com>
2020-04-07 22:21:53 -0400

    correct component imports

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import logo from './logo.svg';
 import './App.css';
-import BusinessList from '../BusinessList';
-import SearchBar from '../SearchBar';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
   return (
