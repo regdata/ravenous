8bcb77df10e80d7c18399319a758c88325ce7b6e
regdata <regdata.voice@gmail.com>
2020-04-11 16:42:48 -0400

    add  functionality for searchbar state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
@@ -19,14 +18,20 @@ const business = {
 
 const businesses = [business, business, business, business, business, business];
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList businesses={businesses} />
-    </div>
-  );
+class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, and ${sortBy}.`);
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={businesses} />
+      </div>
+    );
+  }
 }
 
 export default App;
